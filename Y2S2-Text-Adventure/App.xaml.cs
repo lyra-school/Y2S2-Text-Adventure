ec93df2a3cc7d6a5efbcd82930fda43349e53a1c
lyra-school <s00233718@atu.ie>
2024-03-12 23:00:15 +0000

    Add project files.

---
+++ Y2S2-Text-Adventure/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Y2S2_Text_Adventure
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}

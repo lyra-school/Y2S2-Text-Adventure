25065b59c26b9e076ac33daf4f186167c9b21f74
lyra-school <s00233718@atu.ie>
2024-03-17 15:42:17 +0000

    Added JSON structure + class templates

--- Y2S2-Text-Adventure/MainWindow.xaml.cs
+++ Y2S2-Text-Adventure/MainWindow.xaml.cs
@@ -25,36 +25,66 @@ namespace Y2S2_Text_Adventure
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Changes Main screen to the How to Play screen on click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnHow_Click(object sender, RoutedEventArgs e)
         {
             gridMenu.Visibility = Visibility.Collapsed;
             gridHowto.Visibility = Visibility.Visible;
         }
 
+        /// <summary>
+        /// Changes How to Play screen to Main screen on click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnReturnHow_Click(object sender, RoutedEventArgs e)
         {
             gridMenu.Visibility = Visibility.Visible;
             gridHowto.Visibility = Visibility.Collapsed;
         }
 
+        /// <summary>
+        /// Changes Main screen to the About screen on click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnAbout_Click(object sender, RoutedEventArgs e)
         {
             gridMenu.Visibility = Visibility.Collapsed;
             gridAbout.Visibility = Visibility.Visible;
         }
 
+        /// <summary>
+        /// Changes About screen to the Main screen on click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnReturnAbout_Click(object sender, RoutedEventArgs e)
         {
             gridMenu.Visibility = Visibility.Visible;
             gridAbout.Visibility = Visibility.Collapsed;
         }
 
+        /// <summary>
+        /// Changes Main screen to the Game screen on click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnStart_Click(object sender, RoutedEventArgs e)
         {
             gridMenu.Visibility = Visibility.Collapsed;
             gridGame.Visibility = Visibility.Visible;
         }
 
+        /// <summary>
+        /// Toggles visibility of the statistics list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnStats_Click(object sender, RoutedEventArgs e)
         {
             if(tblkStats.Visibility == Visibility.Visible)
@@ -66,6 +96,11 @@ namespace Y2S2_Text_Adventure
             }
         }
 
+        /// <summary>
+        /// Toggles visibility of the inventory list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnInventory_Click(object sender, RoutedEventArgs e)
         {
             if(tblkInventory.Visibility == Visibility.Visible)
@@ -77,6 +112,12 @@ namespace Y2S2_Text_Adventure
             }
         }
 
+        /// <summary>
+        /// Removes text from the prompt when clicked
+        /// Also changes font to Normal as placeholder text is in Italic
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void tbxPrompt_GotFocus(object sender, RoutedEventArgs e)
         {
             tbxPrompt.FontStyle = FontStyles.Normal;

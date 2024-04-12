084b5926d141d4094aebf0a01fc7e2074a118d76
lyra-school <s00233718@atu.ie>
2024-04-12 13:05:20 +0100

    CodeFirst setup (unfinished)

---
+++ Y2S2-Text-Adventure/Savedata.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Y2S2_Text_Adventure
+{
+    public class Savedata
+    {
+        public int SavedataId { get; set; }
+        public string Name { get; set; }
+        public DateTime DateCreated { get; set; }
+
+        public int Health {  get; set; }
+        public int Will { get; set; }
+        public string SceneName { get; set; }
+
+        public virtual List<SavedataItem> Items { get; set; }
+
+        public Savedata()
+        {
+            Items = new List<SavedataItem>();
+        }
+    }
+}
---
+++ Y2S2-Text-Adventure/SavedataItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Y2S2_Text_Adventure
+{
+    public class SavedataItem
+    {
+        public int SavedataItemId {  get; set; }
+        public string ItemName {  get; set; }
+
+        public int SavedataId { get; set; }
+        public virtual Savedata Save { get; set; }
+    }
+}
